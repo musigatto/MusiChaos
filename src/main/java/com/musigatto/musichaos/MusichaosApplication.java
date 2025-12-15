8be19ec77242607d6dc025289d2af144f5a30675
Lua <maui_195@hotmail.com>
2025-12-15 21:17:24 +0100

    Initial Commit

---
+++ src/main/java/com/musigatto/musichaos/MusichaosApplication.java
@@ -0,0 +1,14 @@
+package com.musigatto.musichaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusichaosApplication {
+
+	public static void main(String[] args) {
+		System.out.printf("hello");
+		SpringApplication.run(MusichaosApplication.class, args);
+	}
+
+}
